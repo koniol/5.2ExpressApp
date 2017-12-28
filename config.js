5cde0cda89bdf4b856646d62dfef00df959d94b2
Paweł Konik <konius85@gmail.com>
2017-12-28 22:15:05 +0100

    Googale auth

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    GOOGLE_CLIENT_ID : '276956496198-66u49altnm0u9rld5vsh0e9u42scibac.apps.googleusercontent.com',
+    GOOGLE_CLIENT_SECRET : 'AIzaSyDC6IyDLYLs1U6E5YX9CbFMLNRyAOLYcvo',
+    CALLBACK_URL : 'http://localhost:4000/auth/google/callback'
+}
--- server.js
+++ server.js
@@ -1,51 +1,55 @@
-var express = require("express");
-var fs = require("fs");
+var express = require('express');
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var config = require('./config');
 var app = express();
+var googleProfile = {};
 
-app.use(express.static("views/static"));
+
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+});
+
+passport.use(new GoogleStrategy({
+        clientID: config.GOOGLE_CLIENT_ID,
+        clientSecret: config.GOOGLE_CLIENT_SECRET,
+        callbackURL: config.CALLBACK_URL
+    },
+    function(accessToken, refreshToken, profile, cb) {
+        googleProfile = {
+            id: profile.id,
+            displayName: profile.displayName
+        };
+        cb(null, profile);
+    }
+));
 
 app.set('view engine', 'pug');
-app.set('views','./views');
-// Dołączenie folderu assets
-// app.use(express.static("assets"));
+app.set('views', './views');
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/first-template', function(req, res){
-    res.render('first-template',
-        {
-            name: "Paweł",
-            age: 34
-        });
+//app routes
+app.get('/', function(req, res){
+    res.render('index', { user: req.user });
 });
 
-app.use('/store', function(req, res, next){
-    console.log('Hej, jestem pośrednikiem między żądaniem a odpowiedzią!');
-    next();
+app.get('/logged', function(req, res){
+    res.render('logged', { user: googleProfile });
 });
+//Passport routes
+app.get('/auth/google',
+    passport.authenticate('google', {
+        scope : ['profile', 'email']
+    }));
+app.get('/auth/google/callback',
+    passport.authenticate('google', {
+        successRedirect : '/logged',
+        failureRedirect: '/'
+    }));
 
-app.get("/store", function (req, res) {
-    console.log("Dostałem się do sklepu");
-    res.send("To jest sklep");
-});
-
-app.get('/', function (req, res) {
-    res.sendFile('./index');
-});
-
-app.get('/userform', function (req, res) {
-    const response = {
-        usr_name: req.query.first_name,
-        las_name: req.query.last_name,
-    };
-    res.end(JSON.stringify(response));
-});
-
-app.get('/google/auth', function (req, res) {
-    res.send("Imie: " + req.query.first_name);
-});
-
-var server = app.listen(4000, 'localhost', function () {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
-});
-
+app.listen(4000);
